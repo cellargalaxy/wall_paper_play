a65377ea110b86d3c116d64a7f742b5e79bd2e9c
lixinzhao <lixinzhao@bytedance.com>
2022-06-17 19:39:21 +0800

    备份

--- c222ca40.py
+++ c222ca40.py
@@ -8,17 +8,13 @@ except ImportError:
     print "Can't import AppKit -- maybe you're running python from brew?"
     print "Try running with Apple's /usr/bin/python instead."
     exit(1)
- 
-from datetime import datetime
+
 from time import sleep
- 
+
 last_active_name = None
 while True:
     active_app = NSWorkspace.sharedWorkspace().activeApplication()
     if active_app['NSApplicationName'] != last_active_name:
         last_active_name = active_app['NSApplicationName']
-        print '%s [%s]' % (
-            active_app['NSApplicationName'],
-            active_app['NSApplicationPath']
-        )
+        print '%s [%s]' % (active_app['NSApplicationName'], active_app['NSApplicationPath'])
     sleep(1)
